d59f363b0299fd1e942aca82ef78e7f152dc7ef7
Lukas Zhao <Lukas Zhao>
2016-12-07 22:18:16 -0500

    more examples

--- main.cpp
+++ main.cpp
@@ -9,5 +9,11 @@ int main()
     complex<double> y(0.45, 0.78);
     cout<<"y="<<y<<endl;
     cout<<"pow(y,0.31)="<<pow(y,0.31)<<endl;
+
+    complex<double> c = pow(y,0.31);
+    cout<<"real part is: "<<c.real()<<endl;
+    cout<<"can also do this:"<<endl;
+    cout<<(pow(y,0.31)).real()<<endl;
+    cout<<(pow(y,0.31)+pow(-y,0.25)).real()<<endl;
     return 0;
 }
