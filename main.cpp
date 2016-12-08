20e08f62adc5c19e0374ad3fb559cdcee907214b
Lukas Zhao <Lukas Zhao>
2016-12-07 21:20:30 -0500

    complex number in C++ example

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    complex<double> y(0.45, 0.78);
+    cout<<"y="<<y<<endl;
+    cout<<"pow(y,0.31)="<<pow(y,0.31)<<endl;
+    return 0;
+}
